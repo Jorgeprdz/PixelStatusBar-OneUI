d697409abd47397ace334e7072673fdd5c63b24e
Jorgeprdz <JORGEPALACIOSRODRIGUEZ@GMAIL.COM>
2026-09-12 16:09:38 -0600

    v0.15: bump safe core version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.jorge.pixelstatusbar.oneui.v4"
         minSdk = 31
         targetSdk = 36
-        versionCode = 15
-        versionName = "0.14-pixel-geometry"
+        versionCode = 16
+        versionName = "0.15-safe-core"
     }
 
     compileOptions {

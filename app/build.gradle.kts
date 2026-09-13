bb0fef9759f7d72961ba445c414bf90abb93009c
Jorgeprdz <JORGEPALACIOSRODRIGUEZ@GMAIL.COM>
2026-09-13 00:26:06 -0600

    v0.18 rescue: bump fail-closed version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.jorge.pixelstatusbar.oneui.v4"
         minSdk = 31
         targetSdk = 36
-        versionCode = 18
-        versionName = "0.17-truthful-core"
+        versionCode = 19
+        versionName = "0.18-rescue-fail-closed"
     }
 
     compileOptions {
